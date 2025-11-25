283e8ce4ab0fcd7d0cad62abd29f95b8313588a3
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-11-25 19:47:33 +0530

    class object

---
+++ OOP/class_obj.java
@@ -0,0 +1,19 @@
+
+package OOP;
+
+class class_obj {
+      int id=101;
+      String name="Dnyaneshwari";
+      double salary=12000;
+
+      void work(){
+        System.out.println("Manager");
+      }
+
+      public static void main(String[] args) {
+        class_obj obj=new class_obj();
+        System.out.println(obj.id);
+        obj.work();
+      }
+    
+}
---
+++ add.java
@@ -0,0 +1,8 @@
+class add{
+    public static void main(String[] str){
+        int a=8;
+        int b=2;
+        int add=a+b;
+        System.out.println(add);
+    }
+}
