c5c43822cb83f2e2ef961f4870111fc98951e0ad
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-11-28 23:24:49 +0530

    String

--- String/demo.java
+++ String/demo.java
@@ -1,5 +1,7 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("heyyy");
+        String str = "Hello world";
+        System.out.println(str.length());
     }
 }
