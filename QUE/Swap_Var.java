85f30a27a97e53ec8d2a41dfdb65578eca3dd957
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-12-01 12:16:49 +0530

    swap number

---
+++ QUE/Swap_Var.java
@@ -0,0 +1,15 @@
+package QUE;
+
+public class Swap_Var {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        a=a+b;
+        b=a-b; //10
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+    
+
+    }
+}
